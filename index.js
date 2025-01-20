0a78c3457960fa876266b673eff90c4abbd99d5b
levisamuel2005 <levi.samuel@kalvium.community>
2025-01-20 11:52:39 +0530

    completed practice assignment 1 on setting up DB

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = 3010;
+const mongoURL = process.env.mongoURI;
 
 app.use(express.static('static'));
+
+mongoose.connect(mongoURL)
+.then(() => console.log('Connected to database'))
+.catch(err => console.log('Error connecting to database:',err));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
